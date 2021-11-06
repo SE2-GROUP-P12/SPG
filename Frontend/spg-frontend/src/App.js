bd0aa1cfda7ca291cb5f67f3e2e456033c58ee74
MARTINA SAUGO s285326 <s285326@studenti.polito.it>
2021-11-06 14:02:02 +0100

    feat: created homepage and navbar

--- Frontend/spg-frontend/src/App.js
+++ Frontend/spg-frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import {Navbar} from "./Navbar";
+import {Homepage} from "./Homepage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Container fluid className="header">
+           <Navbar/>
+            <Homepage/>
+        </Container>
+      </div>
+
   );
 }
 
---
+++ Frontend/spg-frontend/src/Navbar.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+//import {Link} from "react-router-dom";
+import logo from "./resources/logo.png";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+//import {useState} from "react";
+
+function doLogOut()
+{
+    console.log("log out");
+}
+
+function Navbar ()
+{
+    //const [logged, setLogged]=useState(null);
+    const logged=null;
+    return (
+        <Nav className="navbar bg-success navbar-dark">
+        <img src={logo} alt="logo" className="logo" />
+            { logged!=null ? <Button variant="outline-light" onClick={()=>doLogOut()}>Log out</Button> : <Button className="btn btn-outline-light" variant="success"> Log in </Button> }
+        </Nav>
+    );
+}
+
+export {Navbar}
