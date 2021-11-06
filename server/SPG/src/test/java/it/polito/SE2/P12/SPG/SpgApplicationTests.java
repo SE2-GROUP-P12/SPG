1190f97bc5de3cb065131a41e4de03208da65504
MattiaRiola <s280169@studenti.polito.it>
2021-11-06 11:24:01 +0100

    build: spring backbone and gitignore

---
+++ server/SPG/src/test/java/it/polito/SE2/P12/SPG/SpgApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polito.SE2.P12.SPG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
