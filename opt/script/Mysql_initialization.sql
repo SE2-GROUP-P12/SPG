f2bdb8ef0b2a20054eed2835a18a09f5b9e23b99
MattiaRiola <s280169@studenti.polito.it>
2021-11-07 11:22:06 +0100

    fix: mysql database creation

--- opt/script/Mysql_initialization.sql
+++ opt/script/Mysql_initialization.sql
@@ -1,6 +1,5 @@
 BEGIN
 
-CREATE DATABASE `spg` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 CREATE TABLE `basket` (
   `user_id` int NOT NULL,
   `product_id` int NOT NULL,
