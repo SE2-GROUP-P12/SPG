92b76700af32bc95de305ddecf9d13ae0fde40a3
STEFANO GRIVA s287729 <s287729@studenti.polito.it>
2021-11-09 10:55:21 +0100

    Feat: updated pom for sonarcloud and added find product by id query

--- server/SPG/src/main/java/it/polito/SE2/P12/SPG/repository/ProductRepo.java
+++ server/SPG/src/main/java/it/polito/SE2/P12/SPG/repository/ProductRepo.java
@@ -3,10 +3,12 @@ package it.polito.SE2.P12.SPG.repository;
 import it.polito.SE2.P12.SPG.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+@Repository
 public interface ProductRepo
         extends JpaRepository<Product, Integer> {
 
+    @Query(value = "SELECT * FROM product p WHERE p.product_id= ?1")
+    User findProductById(Integer id);
 
 
 }
