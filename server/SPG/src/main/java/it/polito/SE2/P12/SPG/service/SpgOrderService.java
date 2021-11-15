471948598fd2b3e047d7747a66d35ae7e2ce5dee
rayglo <41805546+rayglo@users.noreply.github.com>
2021-11-15 13:46:09 +0100

    Implement Deliver Order

--- server/SPG/src/main/java/it/polito/SE2/P12/SPG/service/SpgOrderService.java
+++ server/SPG/src/main/java/it/polito/SE2/P12/SPG/service/SpgOrderService.java
@@ -3,16 +3,12 @@ package it.polito.SE2.P12.SPG.service;
 import it.polito.SE2.P12.SPG.entity.Basket;
 import it.polito.SE2.P12.SPG.entity.Order;
 import it.polito.SE2.P12.SPG.entity.Product;
-import it.polito.SE2.P12.SPG.entity.User;
 import it.polito.SE2.P12.SPG.repository.OrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class SpgOrderService {
@@ -31,7 +27,6 @@ public class SpgOrderService {
     }
 
     public Boolean addNewOrderFromBasket(Basket basket) {
-        System.out.println(basket.getProductQuantityMap());
         for (Map.Entry<Product, Double> e : basket.getProductQuantityMap().entrySet()) {
             Product p = e.getKey();
             Double q = e.getValue();
@@ -41,8 +36,18 @@ public class SpgOrderService {
         return addNewOrder(order);
     }
 
-    public Boolean deliverOrder(Long userId) {
-        orderRepo.deleteByCust_UserId(userId);
+    public Boolean deliverOrder(Long orderId) {
+        Optional<Order> o = orderRepo.findById(orderId);
+        if(!o.isPresent())
+            return false;
+        Order order = o.get();
+        orderRepo.delete(order);
+        for(Map.Entry<Product, Double> e : order.getProds().entrySet()){
+            Product p = e.getKey();
+            Double q = e.getValue();
+            if(!p.moveFromOrderedToDelivered(q))
+                return false;
+        }
         return true;
     }
 
@@ -51,7 +56,7 @@ public class SpgOrderService {
         for (Order order : orderRepo.findAllByCust_UserId(userId)) {
             List<Product> list = order.getProductList();
             for (Product orderItem : list) {
-                orderItem.setQuantityAvailable(orderRepo.findByOrderId(order.getOrderId()).getProds().get(orderItem));
+                orderItem.setQuantityAvailable(orderRepo.findById(order.getOrderId()).get().getProds().get(orderItem));
             }
             output.add(list);
         }
