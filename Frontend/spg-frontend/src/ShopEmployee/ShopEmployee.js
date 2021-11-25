f87450c80945070af512cbd1aea6c66c596564d9
Giuseppe Gagliardi <giuseppe.gagliardi@studenti.polito.it>
2021-11-25 18:46:03 +0100

    test: deliverOrder #SPG-108

---
+++ Frontend/spg-frontend/src/ShopEmployee/ShopEmployee.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import Button from "react-bootstrap/Button"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {Link} from "react-router-dom";
+
+function ShopEmployee()
+{
+    return(
+        <>
+        <h1>Shop Employee</h1>
+        <div id="container" className="dashboard">
+            <Row>
+                <Col xs={4}><Link to="/NewCustomer"><Button className="dashButton" variant="outline-success"> <h1>New Customer</h1> </Button> </Link></Col>
+                <Col xs={4}><Link to="/BrowseProducts"><Button className="dashButton" variant="outline-success"> <h1>Browse Products</h1> </Button></Link></Col>
+            <Col xs={4}><Link to="/TopUp"><Button className="dashButton" variant="outline-success"> <h1>Top Up</h1> </Button></Link></Col>
+        </Row>
+        <Row>
+            <Col xs={6}><Link to='/PlaceOrder'><Button className="dashButton" variant="outline-success"> <h1>Place Order</h1> </Button></Link></Col>
+            <Col xs={6}><Link to='/DeliverOrder'><Button className="dashButton" variant="outline-success"> <h1>Deliver Order</h1> </Button></Link></Col>
+        </Row>
+    </div></>);
+}
+
+export {ShopEmployee}
---
+++ Frontend/spg-frontend/src/ShopEmployee/ShopEmployee.test.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {getQueriesForElement} from '@testing-library/dom';
+
+import {ShopEmployee} from "./ShopEmployee";
+import { BrowserRouter as Router} from "react-router-dom";
+
+function render(comp) {
+    const root = document.createElement("div");
+    ReactDOM.render(comp, root);
+    return getQueriesForElement(root);
+}
+
+test ("renders correctly", () => {
+
+    const {getByText, getByAltText} = render(
+        <Router>
+            <ShopEmployee/>
+        </Router>
+    );
+
+    getByText("Shop Employee");
+    getByText("New Customer");
+    getByText("Top Up");
+    getByText("Place Order");
+    getByText("Deliver Order");
+    
+
+});
