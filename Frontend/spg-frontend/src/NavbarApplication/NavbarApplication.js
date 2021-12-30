ce4f2f2866056ef42153c28beca63ee22261f77b
Riccardo <s287961@studenti.polito.it>
2021-12-30 16:36:53 +0100

    fix: user story 43 BE and FE alert (#SPG-178)

--- Frontend/spg-frontend/src/NavbarApplication/NavbarApplication.js
+++ Frontend/spg-frontend/src/NavbarApplication/NavbarApplication.js
@@ -29,6 +29,7 @@ function NavbarApplication(props) {
                 sessionStorage.removeItem("refreshToken");
                 localStorage.removeItem("role");
                 localStorage.removeItem("username");
+                localStorage.removeItem("missedPickUp");
             } else {
                 console.log("unsuccesfull logout");
             }
@@ -63,12 +64,33 @@ function NavbarApplication(props) {
                                 delay={{show: 250, hide: 400}}
                                 overlay={<Tooltip id="button-tooltip">{props.topUpWarning.message}</Tooltip>}
                                 placement="left">
-                                <img src={warning} alt="warning" className="warning"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white"
+                                     className="bi bi-exclamation-triangle-fill mt-2 mx-2 rounded bg-danger" viewBox="0 0 16 16" >
+                                    <path
+                                        d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                </svg>
                             </OverlayTrigger>
                             : <></>}
+                        {
+                            props.isLoggedFlag && localStorage.getItem("missedPickUp") !== "0" ?
+                                <OverlayTrigger
+                                    delay={{show: 250, hide: 400}}
+                                    overlay={<Tooltip id="button-tooltip">There
+                                        are {localStorage.getItem("missedPickUp")} order(s) waiting for your pick up,
+                                        please pick it up as soon as possible</Tooltip>}
+                                    placement={"left"}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white"
+                                         className="bi bi-exclamation-triangle-fill mt-2 mx-2 rounded bg-danger" viewBox="0 0 16 16" >
+                                        <path
+                                            d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                    </svg>
+                                </OverlayTrigger>
+                                : <></>
+                        }
                         {location.pathname != "/BrowseProducts" && location.pathname != "/Dashboard" && (props.loggedUserRole === 'CUSTOMER' ||
                             props.loggedUserRole === '') ?
-                            <Button  id='browse-product-button' className="btn btn-outline-light navbar-button" variant="success"
+                            <Button id='browse-product-button' className="btn btn-outline-light navbar-button"
+                                    variant="success"
                                     href='/BrowseProducts'> Browse Products </Button> : <></>}
                         {props.isLoggedFlag && location.pathname != "/Dashboard" ?
                             <Button id='dashboard' className="btn btn-outline-light navbar-button" variant="success"
@@ -80,11 +102,12 @@ function NavbarApplication(props) {
                             props.isLoggedFlag ? <Button id='logout' className="btn btn-danger navbar-button"
                                                          onClick={event => doLogOut(event)}>Log Out</Button> : <>
                                 {location.pathname == "/NewCustomer" ? <></> :
-                                    <Button id='signup' className="btn btn-outline-light navbar-button" variant="success"
+                                    <Button id='signup' className="btn btn-outline-light navbar-button"
+                                            variant="success"
                                             href='/NewCustomer'> Sign
                                         up </Button>}
                                 {location.pathname == "/LoginComponent" ? <></> :
-                                    <Button  id='login' className="btn btn-outline-light navbar-button" variant="success"
+                                    <Button id='login' className="btn btn-outline-light navbar-button" variant="success"
                                             href='/LoginComponent'> Log in </Button>
                                 }
                             </>
