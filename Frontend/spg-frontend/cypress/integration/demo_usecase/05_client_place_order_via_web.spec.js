67dded44bd55ac21f2a3b03a9915d215f39b2a6d
MattiaRiola <s280169@studenti.polito.it>
2022-01-04 19:39:27 +0100

    test(e2e): example of use of seed and teardown functions #SPG-183

--- Frontend/spg-frontend/cypress/integration/demo_usecase/05_client_place_order_via_web.spec.js
+++ Frontend/spg-frontend/cypress/integration/demo_usecase/05_client_place_order_via_web.spec.js
@@ -9,7 +9,14 @@
  * He plans to pick up his order on Wednesday morning.
  */
 context('customer workflow', () => {
+
+    //TODO: seed -> inserisci giovanni
+    //TODO: seed -> inserisci mele, cavolfiori ...
+
     //TODO: test this use case
 
 
+    //TODO: teardown -> (seed) elimina giovanni
+    //TODO: teardown -> (seed) elimina mele, cavolfiori ...
+    //TODO: teardown -> elimina ordine creato nel test
 })
