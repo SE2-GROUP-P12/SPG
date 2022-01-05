1b4b203ec36a34e38336f89bbbb3045bc9264a9d
MattiaRiola <s280169@studenti.polito.it>
2022-01-05 20:23:25 +0100

    test: add usecase 1 test #SPG-183

--- Frontend/spg-frontend/cypress/integration/demo_usecase/01_client_registration_by_employee_and_wallet_charge.spec.js
+++ Frontend/spg-frontend/cypress/integration/demo_usecase/01_client_registration_by_employee_and_wallet_charge.spec.js
@@ -11,8 +11,48 @@ context('customer workflow', () => {
     it('Mario login', () => {
         cy.visit(Cypress.env('baseUrl'));
         cy.login('mario@gmail.com','password');
-        
+        cy.get('#button-ResgisterCustomer').should('be.visible');
     });
-    //TODO: test this use case
+    it('Miriam registration', () => {
+        cy.get('#button-ResgisterCustomer').click();
+        cy.contains('Register').should('exist');
+        cy.signUp(
+            'miriam@gmail.com',
+            'miriam1945',
+            'Miriam',
+            'Sciandra',
+            'Corso Unione Sovietica, 110 10134 Torino TO',
+            'MRMSND45A47G224W'
+        );
+    });
+    it('Miriam logout',() => {
+        cy.logout();
+    })
+    
+    it('Mario login', () => {
+        cy.visit(Cypress.env('baseUrl'));
+        cy.login('mario@gmail.com','password');
+        cy.get('#button-TopUp').should('be.visible');
+    });
+
+    it('Miriam wallet top up', () => {
+        cy.get('#button-TopUp').click();
+        cy.contains('Top Up').should('be.visible');
+        cy.topUp('miriam@gmail.com','50');
+    })
+
+    
+    it('Mario logout',() => {
+        cy.logout();
+    })
+
+    it('teardown Miriam user', () => {
+        const dbName = 'spg';
+        var query = `DELETE FROM spg.customer WHERE address like 'Corso Unione Sovietica, 110 10134 Torino TO';`;
+        cy.task('queryDatabase', { dbName, query });
+        query = "DELETE FROM spg.user WHERE email like 'miriam@gmail.com';";
+        cy.task('queryDatabase', { dbName, query });
+        //wallet operations will be automatically dropped from the db when the user is deleted
+    });
 
 })
--- Frontend/spg-frontend/src/TopUp/TopUp.js
+++ Frontend/spg-frontend/src/TopUp/TopUp.js
@@ -2,20 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {useState} from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import {Formik, Form, Field, FastField} from "formik";
+import { Formik, Form, Field, FastField } from "formik";
 import * as Yup from "yup";
-import {Link} from 'react-router-dom';
-import {API} from "../API/API";
+import { Link } from 'react-router-dom';
+import { API } from "../API/API";
 import Alert from 'react-bootstrap/Alert';
 import Grid from '@mui/material/Grid';
 
-function TopUp()
-{
-    const [user, setUser]=useState(null);
-    const [wallet, setWallet]=useState(0.00);
+function TopUp() {
+    const [user, setUser] = useState(null);
+    const [wallet, setWallet] = useState(0.00);
     const [show, setShow] = useState(false);
     const [error, setError] = useState(null);
     const [success, setSuccess] = useState(null);
@@ -26,119 +25,118 @@ function TopUp()
 
     return (
         <>
-        <h1>Top Up!</h1>
-        <Formik
-            initialValues={{
-                email:''
-            }}
-            validationSchema={
-                Yup.object({
-                    email: Yup.string().email().required()
-                })}
-            onSubmit={async(values) => {
-                setWallet(0);
-                setSuccess(false);
-                setError(false);
-                setModalError(false);
-                setModalSuccess(false);
-                setUser(null);
-                let outcome = await API.getWallet(values.email);
-                if (outcome===undefined)
-                    setError(true);
-                else
-                {   
-                    setWallet(outcome);
-                    setUser(values.email);
-                    setSuccess(true);
-                }
-            }}
-            validateOnChange={false}
-            validateOnBlur={false}
-        >
-            {({values, errors, touched})=>
-            <div id="container" className="pagecontent">
-                <Form>
-                    <Row><label htmlFor="email">Email:</label> <Field id="email" name="email" type="text"/></Row>
-                    <Button style={{margin : '20px'}} type='submit' variant='success'>Submit</Button>
-                    
-                <Row>{errors.email && touched.email ? errors.email : null}</Row>
-                {error ? <Alert variant='danger'>User not found</Alert> : null}
-                {success ? <Alert variant='success'>User correctly found</Alert> : null}
-                {modalError ? <Alert variant='danger'>Something went wrong during the top up</Alert> : null}
-                {modalSuccess ? <Alert variant='success'>Top up correctly performed</Alert> : null}
-                </Form>
-            </div>
-            }
-        </Formik>
-        <h2>User's wallet: {wallet.toFixed(2)}€</h2>
-            { user===null ? 
-            <Grid container spacing={2}>
-            <Grid item xs={6} sm={4}><Button disabled onClick={handleShow} size='huge' variant="outline-success"> <h1>Cash</h1> </Button></Grid>
-            <Grid item xs={6} sm={4}><Button disabled onClick={handleShow} size='huge' variant="outline-success"> <h1>Credit Card</h1> </Button></Grid>
-            <Grid item xs={6} sm={4}><Button disabled onClick={handleShow} size='huge' variant="outline-success"> <h1>Gift Card</h1> </Button></Grid>
-            </Grid>
-             :
-            <Grid container spacing={2}>
-            <Grid item xs={6} sm={4}><Button onClick={handleShow} size='huge' variant="outline-success"> <h1>Cash</h1> </Button></Grid>
-            <Grid item xs={6} sm={4}><Button onClick={handleShow} size='huge' variant="outline-success"> <h1>Credit Card</h1> </Button></Grid>
-            <Grid item xs={6} sm={4}><Button onClick={handleShow} size='huge' variant="outline-success"> <h1>Gift Card</h1> </Button></Grid>
-            </Grid>
-            }
-        <Row>
-            <Link to='/Dashboard'><Button style={{margin: '20px'}} variant='secondary'>Back</Button></Link>
-        </Row>
-    <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Top Up</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Formik
-                    initialValues={{
-                        amount:''
-                    }}
-                    validationSchema={
-                        Yup.object({
-                            amount: Yup.number().required()
+            <h1>Top Up!</h1>
+            <Formik
+                initialValues={{
+                    email: ''
+                }}
+                validationSchema={
+                    Yup.object({
+                        email: Yup.string().email().required()
                     })}
-                    onSubmit={async(values)=>{
-                        setSuccess(false);
-                        setError(false);
-                        setModalError(false);
-                        setModalSuccess(false);
-                        let outcome = await API.topUp(
-                            {"email": user,
-                            "value" : values.amount
-                        });
-                        if(outcome === true)
-                        {
-                            setWallet(wallet+values.amount);
-                            setModalSuccess(true);
-                        }
-                        else
-                            setModalError(true);
-                        handleClose();
-                    }}
-                    validateOnChange={false}
-                    validateOnBlur={false}
-                >
-                    {({values, errors, touched})=>
-                    <div id="container">
-                        <Form>
-                            {errors.amount && touched.amount ? errors.amount : null} <br/>
-                            Amount: <FastField id='amount' type="number" name="amount" label="amount" min="0"
-                                               step="0.01"/> €
-                            <Button style={{margin : '20px'}} type='submit' variant='success'>Top Up</Button>
-                        </Form>                
-                    </div>
+                onSubmit={async (values) => {
+                    setWallet(0);
+                    setSuccess(false);
+                    setError(false);
+                    setModalError(false);
+                    setModalSuccess(false);
+                    setUser(null);
+                    let outcome = await API.getWallet(values.email);
+                    if (outcome === undefined)
+                        setError(true);
+                    else {
+                        setWallet(outcome);
+                        setUser(values.email);
+                        setSuccess(true);
                     }
-                </Formik>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>Close</Button>
-            </Modal.Footer>
-        </Modal>
+                }}
+                validateOnChange={false}
+                validateOnBlur={false}
+            >
+                {({ values, errors, touched }) =>
+                    <div id="container" className="pagecontent">
+                        <Form>
+                            <Row><label htmlFor="email">Email:</label> <Field id="email" name="email" type="text" /></Row>
+                            <Button id='button-Submit' style={{ margin: '20px' }} type='submit' variant='success'>Submit</Button>
+
+                            <Row>{errors.email && touched.email ? errors.email : null}</Row>
+                            {error ? <Alert variant='danger'>User not found</Alert> : null}
+                            {success ? <Alert variant='success'>User correctly found</Alert> : null}
+                            {modalError ? <Alert variant='danger'>Something went wrong during the top up</Alert> : null}
+                            {modalSuccess ? <Alert variant='success'>Top up correctly performed</Alert> : null}
+                        </Form>
+                    </div>
+                }
+            </Formik>
+            <h2>User's wallet: {wallet.toFixed(2)}€</h2>
+            {user === null ?
+                <Grid container spacing={2}>
+                    <Grid item xs={6} sm={4}><Button disabled id='button-Cash' onClick={handleShow} size='huge' variant="outline-success"> <h1>Cash</h1> </Button></Grid>
+                    <Grid item xs={6} sm={4}><Button disabled id='button-Credit Card' onClick={handleShow} size='huge' variant="outline-success"> <h1>Credit Card</h1> </Button></Grid>
+                    <Grid item xs={6} sm={4}><Button disabled id='button-Gift Card' onClick={handleShow} size='huge' variant="outline-success"> <h1>Gift Card</h1> </Button></Grid>
+                </Grid>
+                :
+                <Grid container spacing={2}>
+                    <Grid item xs={6} sm={4}><Button id='button-Cash' onClick={handleShow} size='huge' variant="outline-success"> <h1>Cash</h1> </Button></Grid>
+                    <Grid item xs={6} sm={4}><Button id='button-Credit Card' onClick={handleShow} size='huge' variant="outline-success"> <h1>Credit Card</h1> </Button></Grid>
+                    <Grid item xs={6} sm={4}><Button id='button-Gift Card' onClick={handleShow} size='huge' variant="outline-success"> <h1>Gift Card</h1> </Button></Grid>
+                </Grid>
+            }
+            <Row>
+                <Link to='/Dashboard'><Button style={{ margin: '20px' }} variant='secondary'>Back</Button></Link>
+            </Row>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Top Up</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Formik
+                        initialValues={{
+                            amount: ''
+                        }}
+                        validationSchema={
+                            Yup.object({
+                                amount: Yup.number().required()
+                            })}
+                        onSubmit={async (values) => {
+                            setSuccess(false);
+                            setError(false);
+                            setModalError(false);
+                            setModalSuccess(false);
+                            let outcome = await API.topUp(
+                                {
+                                    "email": user,
+                                    "value": values.amount
+                                });
+                            if (outcome === true) {
+                                setWallet(wallet + values.amount);
+                                setModalSuccess(true);
+                            }
+                            else
+                                setModalError(true);
+                            handleClose();
+                        }}
+                        validateOnChange={false}
+                        validateOnBlur={false}
+                    >
+                        {({ values, errors, touched }) =>
+                            <div id="container">
+                                <Form>
+                                    {errors.amount && touched.amount ? errors.amount : null} <br />
+                                    Amount: <FastField id='amount' type="number" name="amount" label="amount" min="0"
+                                        step="0.01" /> €
+                                    <Button id='button-TopUp' style={{ margin: '20px' }} type='submit' variant='success'>Top Up</Button>
+                                </Form>
+                            </div>
+                        }
+                    </Formik>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 }
 
-export {TopUp}
+export { TopUp }
