ff5c93e112294c342e1c91c421f2ba329addf3b0
MattiaRiola <s280169@studenti.polito.it>
2022-01-05 19:38:09 +0100

    test: fix teardown and seed #SPG-183

--- Frontend/spg-frontend/cypress/integration/demo_usecase/02_client_registration_via_web.spec.js
+++ Frontend/spg-frontend/cypress/integration/demo_usecase/02_client_registration_via_web.spec.js
@@ -1,23 +1,17 @@
 /// <reference types = "Cypress" />
 
-after(() =>{
-    const dbName = 'spg';
-    var query = `DELETE FROM spg.customer WHERE address like 'Corso Duca degli Abruzzi, 24 - 10129 Torino (TO)';`;
-    cy.task('queryDatabase', { dbName, query});
-    query = "DELETE FROM spg.user WHERE email like 'giovanni@gmail.com';";
-    cy.task('queryDatabase', { dbName, query});
-})
+
 
 /**
  * usecase 2
  * on Friday Giovanni, another client, 
  * registers himself with the web site
  */
-context('customer workflow', () => {
-    
+context('Giovanni signup', () => {
+
     //no seeds are needed
-    
-    it('Giovanni Sign up', () =>{
+
+    it('Giovanni Sign up', () => {
         cy.visit(Cypress.env('baseUrl'));
         cy.signUp(
             'giovanni@gmail.com',
@@ -26,24 +20,24 @@ context('customer workflow', () => {
             'Malnati',
             'Corso Duca degli Abruzzi, 24 - 10129 Torino (TO)',
             'MLNGNN80A01L219Q'
-            );
+        );
     })
-    
-    it('Giovanni Login', () =>{
+
+    it('Giovanni Login', () => {
         cy.visit(Cypress.env('baseUrl'));
         cy.logout();
-        cy.login('giovanni@gmail.com','Malnati30L');
+        cy.login('giovanni@gmail.com', 'Malnati30L');
         cy.wait(1000);
         cy.logout();
     })
-    
 
-    
-    // it('time machine example', () => {
-    //     cy.visit(Cypress.env('baseUrl'));
-    //     cy.get('#browse-product-button').click();
-    //     cy.timeMachine(0,10);
-        
-    // });
+
+    it('teardown user Giovanni', () => {
+        const dbName = 'spg';
+        var query = `DELETE FROM spg.customer WHERE address like 'Corso Duca degli Abruzzi, 24 - 10129 Torino (TO)';`;
+        cy.task('queryDatabase', { dbName, query });
+        query = "DELETE FROM spg.user WHERE email like 'giovanni@gmail.com';";
+        cy.task('queryDatabase', { dbName, query });
+    })
 
 })
--- Frontend/spg-frontend/cypress/integration/demo_usecase/05_client_place_order_via_web.spec.js
+++ Frontend/spg-frontend/cypress/integration/demo_usecase/05_client_place_order_via_web.spec.js
@@ -8,14 +8,28 @@
  *  the order is accepted even if he has no money in the wallet. 
  * He plans to pick up his order on Wednesday morning.
  */
-context('customer workflow', () => {
+context('Giovanni place order', () => {
 
-    //TODO: seed -> inserisci giovanni
+    it('seed user Giovanni', () => {
+        //TODO: seed -> inserisci giovanni
+        const dbName = 'spg';
+        var query = `INSERT INTO spg.user (role, user_id, active, email, name, password, phone_number, ssn, surname) VALUES ('CUSTOMER', 84, false, 'giovanni@gmail.com', 'Giovanni', '52ee8459cfe61a1b86d2a5a7c9b5926b1b25ada31b76640782bf6a7493bef2cc', '', 'MLNGNN80A01L219Q', 'Malnati');`;
+        cy.task('queryDatabase', { dbName, query });
+        query = `INSERT INTO spg.customer (address, missed_pick_up, wallet, user_id) VALUES ('Corso Duca degli Abruzzi, 24 - 10129 Torino (TO)', 0, 0, 84);`;
+        cy.task('queryDatabase', { dbName, query });
+    })
     //TODO: seed -> inserisci mele, cavolfiori ...
 
     //TODO: test this use case
 
 
+    it('teardown user Giovanni', () => {
+        const dbName = 'spg';
+        var query = `DELETE FROM spg.customer WHERE address like 'Corso Duca degli Abruzzi, 24 - 10129 Torino (TO)';`;
+        cy.task('queryDatabase', { dbName, query });
+        query = "DELETE FROM spg.user WHERE email like 'giovanni@gmail.com';";
+        cy.task('queryDatabase', { dbName, query });
+    })
     //TODO: teardown -> (seed) elimina giovanni
     //TODO: teardown -> (seed) elimina mele, cavolfiori ...
     //TODO: teardown -> elimina ordine creato nel test
